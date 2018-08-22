03313a7843f8adc6804097cd05edf286aa52bc7a
Felipe Santos <flpsno@hotmail.com>
2018-08-21 22:57:13 -0300

    adicionado o vendor bundle

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,6 +17,13 @@ plugins.push(
     })
 );
 
+
+plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({ 
+        name: 'vendor', 
+        filename: 'vendor.bundle.js'
+}));
+
 if (process.env.NODE_ENV == 'production') {
 
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
@@ -34,7 +41,10 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
